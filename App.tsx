68633070dbd34d942490ff74f96918dae7693794
shaibu <shaibusunnuma@gmail.com>
2022-04-06 01:51:02 +0400

    Changed navigation to TabNavigation

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
 import Search from './AppComponents/Search';
-import {MyTabs} from './AppComponents/Navigation/navigation';
+import {MyTabs} from './AppComponents/Navigation/TabNavigation';
 import {WikiProvider} from './Context/Context';
 
 export default function App() {
