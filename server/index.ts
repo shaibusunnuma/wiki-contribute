19ba6118ed8fe2eafbb4a39b1f01989b2d047d7d
shaibu <shaibusunnuma@gmail.com>
2022-04-13 10:48:49 +0400

    Fixed enpoints to get queries

--- server/index.ts
+++ server/index.ts
@@ -11,3 +11,5 @@ app.use(checkLogin);
 
 app.use('/entity', router);
 app.listen(3000);
+
+//endpoint : http://localhost:3000/entity/edit?id=Q494&labels={"en":"Barack","ar":null}&descriptions={"en":"48th President of the United States","ar":null}&aliases={"en":["Barack Hussein Obama II"]}
