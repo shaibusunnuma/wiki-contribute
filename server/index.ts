32fb34df65340db3f2ecdc1dd1116e5b7118e1ac
shaibu <shaibusunnuma@gmail.com>
2022-04-13 08:00:26 +0400

    Refactored code

--- server/index.ts
+++ server/index.ts
@@ -1,23 +1,16 @@
-// import express from "express";
-// import router from './routes/users.js'
+import express, { Express, Request, Response } from 'express'; 
+import router from './routes/entity';
+import checkLogin from './middlewares/checkLogin';
 
-// const app = express();
+const app: Express = express();
+app.use(express.urlencoded({ extended: true})); //allows for accessing post body
+app.use(express.json()) //allows to parse json info from body
+app.use(checkLogin);
 
-// app.use(express.static('public')) //rendering static pages in folder public
-
-// app.set('view engine', 'ejs'); //set up views engine for html rendering
-
-// app.get('/', (req, res) => {
-//     //res.download("server.js"); // download server
-//     //res.json({success: true});
-//     //res.redirect('/')
-//     res.render('index', {title: 'Website'}) //render an html file
-// })
-
-// app.use(express.urlencoded({ extended: true})); //allows for accessing post body
-// app.use(express.json()) //allows to parse json info from body
-// app.use('/users', router);
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!');
+})
 
 
-// app.listen(3000);
-console.log('this is working');
+app.use('/entity', router);
+app.listen(3000);
