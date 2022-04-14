d20a3da58ddd0a4f91c5a9217df7771701101ab2
shaibu <shaibusunnuma@gmail.com>
2022-04-14 16:44:27 +0400

    Changed server from REST to graphql

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,13 @@
 import express from 'express'; 
-import router from './controllers/entityController';
-import checkLogin from './middlewares/checkLogin';
-
+import { graphqlHTTP } from 'express-graphql';
+import Schema from './schema'
 const app = express();
-app.use(express.urlencoded({ extended: true})); //allows for accessing post body
-app.use(express.json()) //allows to parse json info from body
-app.use(checkLogin);
 
 
-
-app.use('/entity', router);
+app.use('/graphql', graphqlHTTP({
+    schema: Schema,
+    graphiql: true
+}));
 app.listen(3000);
 
 //endpoint : http://localhost:3000/entity/edit?id=Q494&labels={"en":"Barack","ar":null}&descriptions={"en":"48th President of the United States","ar":null}&aliases={"en":["Barack Hussein Obama II"]}
