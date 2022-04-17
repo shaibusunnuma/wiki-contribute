78eecb55462bd552383b9db0183a814e7b1188a1
shaibu <shaibusunnuma@gmail.com>
2022-04-18 00:48:15 +0400

    Changed list compaonent to ListItem

--- front_end/AppComponents/Screens/Feed.tsx
+++ front_end/AppComponents/Screens/Feed.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Text, View, SafeAreaView, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
-import {Entity} from '../CustomTypes'
+import { Text, SafeAreaView, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { ListItem } from "@rneui/base";
 import { WikiContext } from '../../Context';
 
 //@ts-ignore
 const Item = ({ entity, navigation }) => (
-  <TouchableOpacity 
-    onPress={
-      () =>navigation.navigate('Properties',
-      {entity: entity})
-    } 
-    style={styles.item}>
+  <ListItem 
+    Component={TouchableOpacity}
+    containerStyle={styles.item}
+    onPress={() =>navigation.navigate('Properties',{entity: entity})
+    }
+    onLongPress={() => console.log("onLongPress()")}
+  >
     <Text style={styles.title}>{entity.placeLabel.value}</Text>
-  </TouchableOpacity>
+  </ListItem>
 );
 
 
@@ -44,10 +45,13 @@ const styles = StyleSheet.create({
   mainView: {
     flex: 1,
     justifyContent: 'center',
-    alignItems:'center'
+    alignItems:'center',
+    height: '100%',
+    width: '100%',
   },
   item: {
-    backgroundColor: '#f9c2ff',
+    flexDirection: 'row',
+    backgroundColor: 'white',
     padding: 20,
     marginVertical: 1,
     marginHorizontal: 5,
