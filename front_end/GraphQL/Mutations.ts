81e66361c067e6734f987a6c822a300dd978d7a8
shaibu <shaibusunnuma@gmail.com>
2022-04-18 18:54:31 +0400

    Created Graphql mutations

---
+++ front_end/GraphQL/Mutations.ts
@@ -0,0 +1,43 @@
+import { gql } from "@apollo/client";
+
+export const UPDATE_PROPERTY_MUTATION = gql`
+  mutation updateProperty(
+    $username: String!
+    $password: String!
+    $id: String!
+    $property: String!
+    $oldValue: String!
+    $newValue: String!
+  ) {
+    updateProperty(
+      username: $username
+      password: $password
+      id: $id
+      property: $property
+      oldValue: $oldValue
+      newValue: $newValue
+    ) {
+      id
+    }
+  }
+`;
+
+export const CREATE_PROPERTY_MUTATION = gql`
+  mutation addProperty(
+    $username: String!
+    $password: String!
+    $id: String!
+    $property: String!
+    $value: String!
+  ) {
+    addProperty(
+      username: $username
+      password: $password
+      id: $id
+      property: $property
+      value: $oldValue
+    ) {
+      id
+    }
+  }
+`;
