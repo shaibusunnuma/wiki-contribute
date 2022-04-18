403f63b2c0bab3d8513634ca69651b26a44451c3
shaibu <shaibusunnuma@gmail.com>
2022-04-18 17:29:28 +0400

    Wrapped app in ApolloClientProvider

--- front_end/App.tsx
+++ front_end/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -8,28 +9,35 @@ import Navigation from './AppComponents/Navigation';
 
 import { EntityProperties } from './AppComponents/Screens/EntityProperties';
 
+const client = new ApolloClient({
+  uri: 'localhost:3000/graphql',
+  cache: new InMemoryCache()
+});
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <Search />
-      <WikiProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Home"
-            component={Navigation}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen 
-            name="Properties" 
-            component={EntityProperties} 
-            options={{ title: 'Properties' }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-      </WikiProvider>
+      <ApolloProvider client={client}>
+        <Search />
+        <WikiProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Home"
+              component={Navigation}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen 
+              name="Properties" 
+              component={EntityProperties} 
+              options={{ title: 'Properties' }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+        </WikiProvider>
+      </ApolloProvider>
     </SafeAreaProvider>
   );
 }
