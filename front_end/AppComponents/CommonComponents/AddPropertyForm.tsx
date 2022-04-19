778e04b943617e1fb51e359a89a1256c556cf0e8
shaibu <shaibusunnuma@gmail.com>
2022-04-19 18:16:02 +0400

    Button Handlers for edit/create property

--- front_end/AppComponents/CommonComponents/AddPropertyForm.tsx
+++ front_end/AppComponents/CommonComponents/AddPropertyForm.tsx
@@ -1,44 +1,67 @@
-import React from 'react';
+import React from "react";
 import { useMutation } from "@apollo/client";
-import { View, StyleSheet, Pressable, Text} from 'react-native';
-import { FormItem } from 'react-native-form-component';
+import { View, StyleSheet, Pressable, Text } from "react-native";
+import { FormItem } from "react-native-form-component";
+import { CREATE_PROPERTY_MUTATION } from "../../GraphQL/Mutations";
+import { WikiContext } from "../../Context";
 
 export default () => {
-    const [propertyID, setPropertyID] = React.useState('');
-    const [value, setValue] = React.useState('');
-    return (
-        <View style={styles.container}>
-            <FormItem
-            label="Property ID"
-            textInputStyle={styles.input}
-            isRequired
-            showErrorIcon={false}
-            value={propertyID}
-            onChangeText={(propertyID) => setPropertyID(propertyID)}
-            asterik
-            />
-            <FormItem
-            label="Value"
-            textInputStyle={styles.input}
-            isRequired
-            value={value}
-            onChangeText={(value) => setValue(value)}
-            asterik
-            />
-            <Pressable style={styles.button} onPress={()=>console.log('submit')}>
-                <Text style={styles.text}>Submit</Text>
-            </Pressable>
-        </View>
-    );
+  const [propertyID, setPropertyID] = React.useState("");
+  const [value, setValue] = React.useState("");
+  const { username, password, QID } = React.useContext(WikiContext);
+
+  const [addProperty, { error }] = useMutation(CREATE_PROPERTY_MUTATION);
+
+  //TODO: pass in user inputs
+  const createProperty = () => {
+    addProperty({
+      variables: {
+        username: "Shaibu108",
+        password: "Brainiac@108",
+        id: "Q494",
+        property: "P196",
+        value: "Q38",
+      },
+    });
+
+    if (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <FormItem
+        label="Property ID"
+        textInputStyle={styles.input}
+        isRequired
+        showErrorIcon={false}
+        value={propertyID}
+        onChangeText={(propertyID) => setPropertyID(propertyID)}
+        asterik
+      />
+      <FormItem
+        label="Value"
+        textInputStyle={styles.input}
+        isRequired
+        value={value}
+        onChangeText={(value) => setValue(value)}
+        asterik
+      />
+      <Pressable style={styles.button} onPress={createProperty}>
+        <Text style={styles.text}>Submit</Text>
+      </Pressable>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center',
+    justifyContent: "center",
     borderRadius: 5,
     marginTop: 50,
     padding: 20,
-    backgroundColor: '#ffffff',
+    backgroundColor: "#ffffff",
   },
   input: {
     paddingHorizontal: 10,
@@ -47,19 +70,19 @@ const styles = StyleSheet.create({
     borderWidth: 0.5,
   },
   button: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     paddingVertical: 12,
     paddingHorizontal: 30,
     borderRadius: 4,
     elevation: 3,
-    backgroundColor: '#006699',
+    backgroundColor: "#006699",
   },
   text: {
     fontSize: 16,
     lineHeight: 21,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     letterSpacing: 0.25,
-    color: 'white',
+    color: "white",
   },
 });
--- front_end/AppComponents/CommonComponents/EditPropertyForm.tsx
+++ front_end/AppComponents/CommonComponents/EditPropertyForm.tsx
@@ -1,53 +1,77 @@
-import React from 'react';
+import React from "react";
 import { useMutation } from "@apollo/client";
-import { View, StyleSheet, Pressable, Text} from 'react-native';
-import { FormItem } from 'react-native-form-component';
+import { View, StyleSheet, Pressable, Text } from "react-native";
+import { FormItem } from "react-native-form-component";
+import { WikiContext } from "../../Context";
+import { UPDATE_PROPERTY_MUTATION } from "../../GraphQL/Mutations";
 
 export default () => {
-    const [propertyID, setPropertyID] = React.useState('');
-    const [oldValue, setOldValue] = React.useState('');
-    const [newValue, setNewValue] = React.useState('');
-    return (
-        <View style={styles.container}>
-            <FormItem
-            label="Property ID"
-            textInputStyle={styles.input}
-            isRequired
-            showErrorIcon={false}
-            value={propertyID}
-            onChangeText={(propertyID) => setPropertyID(propertyID)}
-            asterik
-            />
-            <FormItem
-            label="Old Value"
-            textInputStyle={styles.input}
-            isRequired
-            value={oldValue}
-            onChangeText={(value) => setOldValue(value)}
-            asterik
-            />
-            <FormItem
-            label="New Value"
-            textInputStyle={styles.input}
-            isRequired
-            value={newValue}
-            onChangeText={(value) => setNewValue(value)}
-            asterik
-            />
-            <Pressable style={styles.button} onPress={()=>console.log('submit')}>
-                <Text style={styles.text}>Submit</Text>
-            </Pressable>
-        </View>
-    );
+  const [propertyID, setPropertyID] = React.useState("");
+  const [oldValue, setOldValue] = React.useState("");
+  const [newValue, setNewValue] = React.useState("");
+  const { username, password, QID } = React.useContext(WikiContext);
+
+  const [updateProperty, { error }] = useMutation(UPDATE_PROPERTY_MUTATION);
+
+  //TODO: pass user inputs
+  const editProperty = () => {
+    updateProperty({
+      variables: {
+        username: "Shaibu108",
+        password: "Brainiac@108",
+        id: "Q494",
+        property: "P196",
+        oldValue: "Q193995",
+        newValue: "Q38",
+      },
+    });
+
+    if (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <FormItem
+        label="Property ID"
+        textInputStyle={styles.input}
+        isRequired
+        showErrorIcon={false}
+        value={propertyID}
+        onChangeText={(propertyID) => setPropertyID(propertyID)}
+        asterik
+      />
+      <FormItem
+        label="Old Value"
+        textInputStyle={styles.input}
+        isRequired
+        value={oldValue}
+        onChangeText={(value) => setOldValue(value)}
+        asterik
+      />
+      <FormItem
+        label="New Value"
+        textInputStyle={styles.input}
+        isRequired
+        value={newValue}
+        onChangeText={(value) => setNewValue(value)}
+        asterik
+      />
+      <Pressable style={styles.button} onPress={editProperty}>
+        <Text style={styles.text}>Submit</Text>
+      </Pressable>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center',
+    justifyContent: "center",
     borderRadius: 5,
     marginTop: 50,
     padding: 20,
-    backgroundColor: '#ffffff',
+    backgroundColor: "#ffffff",
   },
   input: {
     paddingHorizontal: 10,
@@ -56,19 +80,19 @@ const styles = StyleSheet.create({
     borderWidth: 0.5,
   },
   button: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     paddingVertical: 12,
     paddingHorizontal: 30,
     borderRadius: 4,
     elevation: 3,
-    backgroundColor: '#006699',
+    backgroundColor: "#006699",
   },
   text: {
     fontSize: 16,
     lineHeight: 21,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     letterSpacing: 0.25,
-    color: 'white',
+    color: "white",
   },
 });
