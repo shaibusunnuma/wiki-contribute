6b06ded926f55f2e582dee4983ac6f44668cf20e
shaibu <shaibusunnuma@gmail.com>
2022-04-19 18:26:38 +0400

    Edited  mutations query

--- front_end/GraphQL/Mutations.ts
+++ front_end/GraphQL/Mutations.ts
@@ -23,21 +23,15 @@ export const UPDATE_PROPERTY_MUTATION = gql`
 `;
 
 export const CREATE_PROPERTY_MUTATION = gql`
-  mutation addProperty(
-    $username: String!
-    $password: String!
-    $id: String!
-    $property: String!
-    $value: String!
-  ) {
-    addProperty(
-      username: $username
-      password: $password
-      id: $id
-      property: $property
-      value: $oldValue
-    ) {
+  mutation addProperty($username: String!, $password: String!, $id: String!, $property: String!, $value: String!)
+  {
+    addProperty(username: $username, password: $password, id: $id, property: $property, value: $value)
+    {
+      username
+      password
       id
+      property
+      value
     }
   }
 `;
--- server/schema/type_defs/propertyCreationType.ts
+++ server/schema/type_defs/propertyCreationType.ts
@@ -1,8 +1,8 @@
-import {GraphQLObjectType, GraphQLString } from 'graphql';
+import { GraphQLObjectType, GraphQLString } from 'graphql';
 
 export default new GraphQLObjectType({
   name: 'AddProperty',
-  description: 'This represents a Wikidata property',
+  description: 'This creates a Wikidata property',
   fields: () => ({
     username: { type: GraphQLString },
     password: { type: GraphQLString },
