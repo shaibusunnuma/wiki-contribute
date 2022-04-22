837a22dbe40d701e510ed1d5228503603e593ac3
shaibu <shaibusunnuma@gmail.com>
2022-04-22 06:05:15 +0400

    Display open new window for links and coordinates

--- front_end/AppComponents/CommonComponents/PropertyListItem.tsx
+++ front_end/AppComponents/CommonComponents/PropertyListItem.tsx
@@ -1,49 +1,95 @@
 //@ts-nocheck
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity} from 'react-native';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Linking,
+} from "react-native";
+import { MaterialCommunityIcons, Ionicons } from "@expo/vector-icons";
 import { ListItem, Card } from "@rneui/base";
+import openMap from "react-native-open-maps";
 
 //@ts-ignore
 export default ({ property, setModalType, setIsModalVisible }) => {
-    const Value = (property.wdLabel.value ===  'image') 
-      ? <ImageValue value={property.ps_Label.value} /> : <TextValue value={property.ps_Label.value} />
+  let Value: JSX.Element;
+  if (property.wdLabel.value === "image") {
+    Value = <ImageValue value={property.ps_Label.value} />;
+  } else if (property.wdLabel.value === "coordinate location") {
+    let value = property.ps_Label.value;
+    value = value.substring(0, value.length - 1);
+    const coord = value.split("(")[1].split(" ");
+    Value = <MapValue value={coord} />;
+  } else if (property.wdLabel.value === "official website") {
+    Value = <LinkValue value={property.ps_Label.value} />;
+  } else {
+    Value = <TextValue value={property.ps_Label.value} />;
+  }
 
-    const toggleModal = () => {
-      setModalType('edit');
-      setIsModalVisible(true);
-    };
+  const toggleModal = () => {
+    setModalType("edit");
+    setIsModalVisible(true);
+  };
 
-    return (
+  return (
     <ListItem containerStyle={styles.itemContainer}>
-        <Card containerStyle={styles.card}>
-            <View style={styles.top}>
-                <Text style={styles.property}>{property.wdLabel.value}</Text>
-                <TouchableOpacity style={styles.iconContainer} onPress={toggleModal}>
-                    <MaterialCommunityIcons name="pencil" size={18} color="black" />
-                </TouchableOpacity>
-            </View>
-            <Card.Divider />
-            {Value}
-        </Card>
+      <Card containerStyle={styles.card}>
+        <View style={styles.top}>
+          <Text style={styles.property}>{property.wdLabel.value}</Text>
+          <TouchableOpacity style={styles.iconContainer} onPress={toggleModal}>
+            <MaterialCommunityIcons name="pencil" size={18} color="black" />
+          </TouchableOpacity>
+        </View>
+        <Card.Divider />
+        {Value}
+      </Card>
     </ListItem>
-    )
-  
+  );
 };
 //@ts-ignore
-const TextValue = ({value}) => (<Text style={styles.value}>{value}</Text>)
+const TextValue = ({ value }) => <Text style={styles.value}>{value}</Text>;
 
 //@ts-ignore
 const ImageValue = ({ value }) => (
-    <Card.Image
-        style={{ padding: 0 }}
-        source={{
-            uri:value,
-        }}
-    />
-)
+  <Card.Image
+    style={{ padding: 0 }}
+    source={{
+      uri: value,
+    }}
+  />
+);
+const MapValue = ({ value }) => (
+  <View style={styles.top}>
+    <Text>{`Lat: ${value[0]}   Long: ${value[1]}`}</Text>
+    <TouchableOpacity
+      style={styles.iconContainer}
+      onPress={() => {
+        openMap({ latitude: +value[0], longitude: +value[1] });
+        console.log(+value[1]);
+      }}
+    >
+      <Ionicons name="open-outline" size={15} color="black" />
+    </TouchableOpacity>
+  </View>
+);
 
-
+const LinkValue = ({ value }) => (
+  <View style={styles.top}>
+    <Text>{value}</Text>
+    <TouchableOpacity
+      style={styles.iconContainer}
+      onPress={async () => {
+        const supported = await Linking.canOpenURL(value);
+        if (supported) {
+          await Linking.openURL(value);
+        }
+      }}
+    >
+      <Ionicons name="open-outline" size={15} color="black" />
+    </TouchableOpacity>
+  </View>
+);
 
 const styles = StyleSheet.create({
   itemContainer: {
@@ -55,8 +101,8 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   card: {
-      width: '100%',
-      margin: 0
+    width: "100%",
+    margin: 0,
   },
   property: {
     fontSize: 13,
@@ -65,13 +111,13 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   top: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   iconContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  }
-})
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+});
