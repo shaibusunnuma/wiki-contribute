72f200f6245e6615708206c4309eb6bc611580ad
shaibu <shaibusunnuma@gmail.com>
2022-04-24 08:54:39 +0400

    Edit Profile form

---
+++ front_end/AppComponents/CommonComponents/EditProfileForm.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { StyleSheet, View, Text, Pressable } from "react-native";
+import { FormItem } from "react-native-form-component";
+import { WikiContext } from "../../Context";
+
+export default function EditProfileForm() {
+  const { username, password, setUsername, setPassword } =
+    React.useContext(WikiContext);
+  const [userNameValue, setUserNameValue] = React.useState("");
+  const [passwordValue, setPasswordValue] = React.useState("");
+  const save = () => {
+    setUsername(userNameValue);
+    setPassword(passwordValue);
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.info}>
+        <Text>
+          Username and password are cached on your device and used to make
+          edits.
+        </Text>
+      </View>
+      <View>
+        <FormItem
+          label="Wikidata Username"
+          textInputStyle={styles.input}
+          showErrorIcon={false}
+          value={userNameValue}
+          onChangeText={(value) => setUserNameValue(value)}
+        />
+        <FormItem
+          label="WikiData password"
+          textInputStyle={styles.input}
+          value={passwordValue}
+          onChangeText={(value) => setPasswordValue(value)}
+        />
+        <Pressable style={styles.button} onPress={save}>
+          <Text style={styles.text}>Save</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    borderRadius: 5,
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: "#E6E6E6",
+  },
+  input: {
+    paddingHorizontal: 10,
+    height: 43,
+    margin: 0,
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "#006699",
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
+  },
+  info: {
+    justifyContent: "center",
+    paddingBottom: 10,
+  },
+});
